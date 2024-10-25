ab48f264fd96bdb86a37be53ec6531f7c1909125
raulodev <raulodev@gmail.com>
2024-10-25 10:35:06 -0400

    create BcPaymentTransactionInfoPlugin

---
+++ src/main/java/org/killbill/billing/plugin/bigcommerce/BcPaymentTransactionInfoPlugin.java
@@ -0,0 +1,46 @@
+package org.killbill.billing.plugin.bigcommerce;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.killbill.billing.catalog.api.Currency;
+import org.killbill.billing.payment.api.PluginProperty;
+import org.killbill.billing.payment.api.TransactionType;
+import org.killbill.billing.payment.plugin.api.PaymentPluginStatus;
+import org.killbill.billing.plugin.api.payment.PluginPaymentTransactionInfoPlugin;
+
+public class BcPaymentTransactionInfoPlugin extends PluginPaymentTransactionInfoPlugin {
+
+    public BcPaymentTransactionInfoPlugin(
+            UUID kbPaymentId,
+            UUID kbTransactionPaymentPaymentId,
+            TransactionType transactionType,
+            BigDecimal amount,
+            Currency currency,
+            PaymentPluginStatus pluginStatus,
+            String gatewayError,
+            String gatewayErrorCode,
+            String firstPaymentReferenceId,
+            String secondPaymentReferenceId,
+            DateTime createdDate,
+            DateTime effectiveDate,
+            List<PluginProperty> properties) {
+
+        super(kbPaymentId,
+                kbTransactionPaymentPaymentId,
+                transactionType,
+                amount,
+                currency,
+                pluginStatus,
+                gatewayError,
+                gatewayErrorCode,
+                firstPaymentReferenceId,
+                secondPaymentReferenceId,
+                createdDate,
+                effectiveDate,
+                properties);
+    }
+
+}
