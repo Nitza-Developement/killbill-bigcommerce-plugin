950907873862dfb1e7c083023f705ae6f6b348ee
raulodev <raulodev@gmail.com>
2024-10-26 17:35:43 -0400

    some changes

--- src/main/java/org/killbill/billing/plugin/bigcommerce/BcConfigurationHandler.java
+++ src/main/java/org/killbill/billing/plugin/bigcommerce/BcConfigurationHandler.java
@@ -11,18 +11,18 @@ import org.slf4j.LoggerFactory;
  * When per-tenant config changes are made, the plugin automatically gets notified (and prints a log trace)
  * <pre>
  * {@code
- * curl -v \
- *      -X POST \
- *      -u admin:password \
- *      -H "Content-Type: text/plain" \
- *      -H "X-Killbill-ApiKey: bob" \
- *      -H "X-Killbill-ApiSecret: lazar" \
- *      -H "X-Killbill-CreatedBy: demo" \
- *      -d 'key1=foo1
- * key2=foo2' \
- *      "http://127.0.0.1:8080/1.0/kb/tenants/uploadPluginConfig/hello-world-plugin"
- * }
- * </pre>
+  curl -v \
+       -X POST \
+       -u admin:password \
+       -H "Content-Type: text/plain" \
+       -H "X-Killbill-ApiKey: v1" \
+       -H "X-Killbill-ApiSecret: v1" \
+       -H "X-Killbill-CreatedBy: demo" \
+       -d 'key1=foo1
+  key2=foo2' \
+       "http://127.0.0.1:8080/1.0/kb/tenants/uploadPluginConfig/bigcommerce-plugin"
+  }
+
  */
 public class BcConfigurationHandler extends PluginTenantConfigurableConfigurationHandler<Properties> {
 
