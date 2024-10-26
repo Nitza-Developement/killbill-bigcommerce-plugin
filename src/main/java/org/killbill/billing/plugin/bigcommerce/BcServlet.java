09c1fde5e9c7ccb1bb7193530834603c9f140e61
raulodev <raulodev@gmail.com>
2024-10-26 17:58:09 -0400

    update

--- src/main/java/org/killbill/billing/plugin/bigcommerce/BcServlet.java
+++ src/main/java/org/killbill/billing/plugin/bigcommerce/BcServlet.java
@@ -8,9 +8,9 @@ import org.jooby.mvc.GET;
 import org.jooby.mvc.Local;
 import org.jooby.mvc.Path;
 import org.killbill.billing.tenant.api.Tenant;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 @Singleton
 @Path("/")
@@ -18,29 +18,38 @@ public class BcServlet {
 
     private static final Logger logger = LoggerFactory.getLogger(BcServlet.class);
 
-    public BcServlet() {
+    public BcServlet(
+
+    ) {
     }
 
     /**
-     * Kill Bill automatically injects Tenant object in this method when this end point is accessed with the X-Killbill-ApiKey and X-Killbill-ApiSecret headers 
+     * Kill Bill automatically injects Tenant object in this method when this end
+     * point is accessed with the X-Killbill-ApiKey and X-Killbill-ApiSecret headers
+     * 
      * @param tenant
      */
     @GET
-    public void hello(@Local @Named("killbill_tenant") final Optional<Tenant> tenant) {
+    public void get(
+            @Local @Named("killbill_tenant") final Optional<Tenant> tenant,
+            @Named("url") final Optional<String> url
+
+    ) {
         // Find me on http://127.0.0.1:8080/plugins/hello-world-plugin
         logger.info("Hello world");
 
-        if(tenant != null && tenant.isPresent() ) {
+        if (tenant != null && tenant.isPresent()) {
 
-        	logger.info("tenant is available");
+            logger.info("tenant is available");
 
-        	Tenant t1 = tenant.get();
+            Tenant t1 = tenant.get();
 
-        	logger.info("tenant id:"+t1.getId());
+            logger.info("tenant id:" + t1.getId());
+        } else {
+
+            logger.info("tenant is not available");
         }
-        else {
-            
-        	logger.info("tenant is not available");
-        }
+
     }
+
 }
